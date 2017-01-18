938d7516450f200581fa29babad2bfc0bdbc396b
Christer Larsen <christer.larsen@crayon.com>
2017-01-18 01:31:12 +0100

    adding stuff

---
+++ src/lib/propchildren.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+export class Propchildren extends Component {
+	render() {
+		return (
+			<Button>I <Heart/> React</Button>
+		);
+	}
+}
+
+const Button = (props) => <button>{props.children}</button>
+
+
+class Heart extends Component {
+	render() {
+		return (
+			<span>&hearts;</span>
+
+		);
+	}
+}
+
+export default Propchildren;
